c4836e381b87fbfc08bf5548aa597d7b7874a9f1
cremerfc <cremerfc@users.noreply.github.com>
2025-07-09 14:05:56 -0400

    Update variables.tf

--- {{ cookiecutter.project_name }}/modules/ec2s/{{ cookiecutter.resource_name }}/variables.tf
+++ {{ cookiecutter.project_name }}/modules/ec2s/{{ cookiecutter.resource_name }}/variables.tf
@@ -2,3 +2,8 @@ variable "cortex_callback" {
   description = "Cortex CallbackURL"
   default     = "http://api.getcortexapp.com"
 }
+
+variable "cortex_token" {
+  description = "Cortex API Token"
+  default     = "123"
+}
